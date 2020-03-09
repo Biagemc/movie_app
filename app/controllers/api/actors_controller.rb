da07c37668f845fff9e59613f8f9a5ac074f5b5f
biagemc <biagemc@gmail.com>
2020-03-08 21:29:41 -0400

    add CRUD to Actors DB

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -4,9 +4,36 @@ class Api::ActorsController < ApplicationController
     render "index.json.jb"
   end
 
-  def show
-    the_id = params[:id]
-    @actor = Actor.find_by(id: the_id)
+  def create
+    @actor = Actor.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for],
+    )
+    @actor.save
     render "show.json.jb"
+  end
+
+  def show
+    @actor = Actor.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @actor = Actor.find_by(id: params[:id])
+    @actor.update(
+      id: params[:id] || @actor.id,
+      first_name: params[:first_name] || @actor.first_name,
+      last_name: params[:last_name] || @actor.last_name,
+      known_for: params[:known_for] || @actor.known_for,
+    )
+    @actor.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @actor = Actor.find_by(id: params[:id])
+    @actor.destroy
+    render "destroy.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/actors" => "actors#index"
+    post "/actors" => "actors#create"
     get "/actors/:id" => "actors#show"
+    patch "/actors/:id" => "actors#update"
+    delete "/actors/:id" => "actors#destroy"
     get "/movies" => "movies#index"
     get "/movies/:id" => "movies#show"
   end
