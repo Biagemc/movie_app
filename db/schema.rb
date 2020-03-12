b90de82e4f8631f31e5d4640d569d3c196634be7
biagemc <biagemc@gmail.com>
2020-03-11 23:14:16 -0400

    add column director + english?

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_024903) do
+ActiveRecord::Schema.define(version: 2020_03_12_025623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2020_03_08_024903) do
     t.text "plot"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "director"
+    t.boolean "english", default: true
   end
 
 end
