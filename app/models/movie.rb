b4ce11187359018df3f23bd0b03db335a96ae974
biagemc <biagemc@gmail.com>
2020-03-24 14:39:20 -0400

    add Genre model + MovieGenre model + links + add movie genre to movie json

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,6 @@ class Movie < ApplicationRecord
   validates :director, length: { minimum: 2 }, presence: true
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 end
