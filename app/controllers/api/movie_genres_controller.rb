80feb13727a359d523bd6b302ee67d6a2777fe70
biagemc <biagemc@gmail.com>
2020-03-26 15:09:24 -0400

    update create action for Movie_genre + add show view

--- app/controllers/api/movie_genres_controller.rb
+++ app/controllers/api/movie_genres_controller.rb
@@ -1,10 +1,10 @@
 class Api::MovieGenresController < ApplicationController
   def create
-    @movie = Movie.new(
+    @movie_genre = MovieGenre.new(
       movie_id: params[:movie_id],
       genre_id: params[:genre_id],
     )
-    if movie.save
+    if @movie_genre.save
       render "show.json.jb"
     else
       render json: { errors: @movie.errors.full_messages }, status: :bad_request
